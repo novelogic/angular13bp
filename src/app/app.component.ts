73aea04a391b83e10dd1f305aa476756858c0680
novelogic <novelogic2017@outlook.com>
2022-03-31 16:24:53 +0200

    Introduced Rxjs , Used Of and From from RxJS to test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent  implements OnInit {
+
+ngOnInit(): void {
+  of(13,14,15,16).subscribe(val => console.log(val),
+  err => console.log(err),
+  () => console.log('Observable completed'));
+
+  from([21,22,23,24,25]).subscribe(
+    {
+      next : (val) => console.log(val),
+      error : err => console.log(err),
+      complete: () => console.log('Observable completed')
+  }
+  );
+}
   aName = 'Angular ' + VERSION.major;
 }
