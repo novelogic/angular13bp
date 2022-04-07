a69b7d0dd47ad510bb49a7780e924b61aa184798
novelogic <novelogic2017@outlook.com>
2022-04-07 19:22:57 +0200

    map , tap and take operaters from rxjs-7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, VERSION } from '@angular/core';
-import { from, of } from 'rxjs';
+import { from, of, map, tap, take } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -9,22 +9,36 @@ import { from, of } from 'rxjs';
 export class AppComponent  implements OnInit {
 
 ngOnInit(): void {
-  of(13,14,15,16).subscribe(val => console.log(val),
+ /* of(13,14,15,16).subscribe(val => console.log(val),
   err => console.log(err),
   () => console.log('Observable completed'));
-
-  from([21,22,23,24,25]).subscribe(
+*/  
+  from([21,22,23,24,15])
+  .pipe(
+    map(value => value *2),
+    map(value => value -30),
+    map(item => {
+      if (item === 0)
+       throw new Error('Hit ti invalid zero value');
+      else
+        return item;
+    }),
+    take(5),
+    tap(value => console.log(value))
+  )
+  .subscribe(
     {
       next : (val) => console.log(val),
-      error : err => console.log(err),
+      error : err => console.error(`the error object is ${err}`),
       complete: () => console.log('Observable completed')
   }
   );
+  /*
   of('Apple', 'Banana', 'pear').subscribe({
     next: item => console.log(`fruit is ${item}`),
     error: err => console.log(`error is ${err}`),
     complete: () => console.log('Observable is Completed.')
-  });
+  });*/
 }
   aName = 'Angular ' + VERSION.major;
 }
